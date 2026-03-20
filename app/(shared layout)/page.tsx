7bc56f0924503165ed7981fb84becfb34a53b95c
= <=>
2026-03-20 06:26:42 +0530

    homepage ui done

--- app/(shared layout)/page.tsx
+++ app/(shared layout)/page.tsx
@@ -1,5 +1,20 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function HomePage() {
-    return (
-        <div>Hello form the index page</div>
-    );
+  return (
+    <section className="py-12">
+      <div className="text-center">
+        <h2 className="text-3xl md:text-5xl font-bold mb-4">
+          Welcome to My Blog
+        </h2>
+        <p className="text-gray-600 max-w-xl mx-auto mb-6">
+          Read amazing articles on tech, coding, and more.
+        </p>
+        <Link href="/blogs" className={buttonVariants({ size: "lg" })}>
+          Explore
+        </Link>
+      </div>
+    </section>
+  );
 }
--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -1,47 +1,128 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import {Button, buttonVariants} from "@/components/ui/button";
-import {ThemeToggle} from "@/components/web/theme-toggle";
-import {useConvexAuth} from "convex/react";
-import {authClient} from "@/lib/auth-client";
-import {toast} from "sonner";
-import {useRouter} from "next/navigation";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/web/theme-toggle";
+import { useConvexAuth } from "convex/react";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 import SearchInput from "@/components/web/SearchInput";
+import { BookOpen, LogIn, SquarePen, UserPlus } from "lucide-react";
 
-export function Navbar(){
-    const {isLoading, isAuthenticated} = useConvexAuth();
-    const router = useRouter();
-    return <nav className="w-full py-5 flex items-center justify-between">
-        <Link href="/">
-            <div className="flex items-center gap-8">
-                <h1 className="text-2xl sm:text-3xl font-bold">Next<span className="text-primary">Pro</span></h1>
-            </div>
-        </Link>
-        <div className="flex items-center gap-1 md:gap-2">
-            <div className="hidden sm:block"><Link className={buttonVariants({variant: "ghost"})} href="/">Home</Link></div>
-            <Link className={buttonVariants({variant: "ghost"})} href="/blogs">Blogs</Link>
-            <Link className={buttonVariants({variant: "ghost"})} href="/create">Create</Link>
+export function Navbar() {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+  const router = useRouter();
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <Link href="/">
+        <div className="flex items-center gap-8">
+          <h1 className="text-2xl sm:text-3xl font-bold">
+            Next<span className="text-primary">Pro</span>
+          </h1>
         </div>
-        <div className="flex items-center gap-2">
-            <div className="hidden md:block">
-                <SearchInput />
-            </div>
-            {
-                isLoading ? null : isAuthenticated ? <Button onClick={()=>authClient.signOut({fetchOptions: {
-                    onSuccess: () => {
-                        toast.success("Logged out successfully")
-                        router.push("/")
-                    },
-                    onError: (error) =>{
-                    toast.error(error.error.message)
-                    }
-                    }})}>Logout</Button> : (<>
-                    <Link className={buttonVariants()} href="/auth/sign-up">Sign Up</Link>
-                    <Link className={buttonVariants({variant: "outline"})} href="/auth/login">Login</Link>
-                </>)
+      </Link>
+      <div className="flex items-center gap-1 md:gap-2">
+        <div className="hidden md:flex">
+          <Link className={buttonVariants({ variant: "ghost" })} href="/">
+            Home
+          </Link>
+        </div>
+        <div>
+          {/* on desktop */}
+          <div className="hidden md:flex">
+            <Link
+              className={buttonVariants({
+                variant: "ghost",
+              })}
+              href="/blogs"
+            >
+              Blogs
+            </Link>
+          </div>
+          {/* on mobile */}
+          <div className="md:hidden">
+            <Link
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+              href="/blogs"
+            >
+              <BookOpen />
+            </Link>
+          </div>
+        </div>
+        {/* on desktop */}
+        <div className="hidden md:flex">
+          <Link className={buttonVariants({ variant: "ghost" })} href="/create">
+            Create
+          </Link>
+        </div>
+        <div className="md:hidden">
+          <Link
+            className={buttonVariants({ variant: "ghost", size: "sm" })}
+            href="/create"
+          >
+            <SquarePen />
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+        {isLoading ? null : isAuthenticated ? (
+          <Button
+            onClick={() =>
+              authClient.signOut({
+                fetchOptions: {
+                  onSuccess: () => {
+                    toast.success("Logged out successfully");
+                    router.push("/");
+                  },
+                  onError: (error) => {
+                    toast.error(error.error.message);
+                  },
+                },
+              })
             }
-            <ThemeToggle />
-        </div>
+          >
+            Logout
+          </Button>
+        ) : (
+          <>
+            {/* on desktop */}
+            <div className="hidden md:flex gap-2">
+              <Link className={buttonVariants()} href="/auth/sign-up">
+                Sign Up
+              </Link>
+              <Link
+                className={buttonVariants({ variant: "outline" })}
+                href="/auth/login"
+              >
+                Login
+              </Link>
+            </div>
+            {/* on mobile */}
+            <div className="flex gap-2 md:hidden">
+              <Link
+                className={buttonVariants({ size: "sm" })}
+                href="/auth/sign-up"
+              >
+                <UserPlus />
+              </Link>
+              <Link
+                className={buttonVariants({ variant: "outline", size: "sm" })}
+                href="/auth/login"
+              >
+                <LogIn />
+              </Link>
+            </div>
+          </>
+        )}
+        <ThemeToggle />
+      </div>
     </nav>
+  );
 }
